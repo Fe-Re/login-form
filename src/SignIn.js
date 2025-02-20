e18bb4b1663342c2774f403809660227d14466e3
Fe-Re <Felixreisenhauer@googlemail.com>
2025-02-20 17:35:15 +0100

    Add password visibility toggle to SignIn component and update styles

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,6 +1,15 @@
 import Icons from "./Icons";
+import eye from "./image/eye.png";
+import eyeClosed from "./image/eye-closed.png";
+import { useState } from "react";
 
 export function SignIn() {
+  const [pwVisible, setPwVisible] = useState(false);
+
+  function togglePwVisibility() {
+    setPwVisible(!pwVisible);
+  }
+
   return (
     <span className="sign-in">
       <h1>Sign In</h1>
@@ -12,7 +21,17 @@ export function SignIn() {
         </label>
         <br />
         <label>
-          <input type="password" placeholder="Password" />
+          <input
+            type={pwVisible ? "text" : "password"}
+            placeholder="Password"
+          />
+          <img
+            className="eye"
+            src={pwVisible ? eyeClosed : eye}
+            width="25px"
+            alt=""
+            onClick={togglePwVisibility}
+          />
         </label>
         <br />
         <a href="/">Forget your password?</a>
